6555f828faca94cda6a42c10c427303b9a920d6f
Tomas Vink <tv@eenwereldmetlef.be>
2018-09-10 17:43:57 +0200

    dependency version changes

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: 'Translations/CMS for Meteor/React',
-  version: '2.2.0',
+  version: '2.2.1',
   name: 'lef:translations'
 })
 
@@ -13,11 +13,10 @@ Package.onUse(api => {
 Npm.depends({
   react: '16.5.0',
   lodash: '4.17.5',
-  'markdown-it': '8.4.1',
-  'markdown-it-video': '0.6.2',
   '@fortawesome/fontawesome': '1.1.8',
   '@fortawesome/fontawesome-svg-core': '1.2.0',
   '@fortawesome/free-solid-svg-icons': '5.2.0',
   '@fortawesome/react-fontawesome': '0.1.0',
-  'markdown-it': '8.4.2'
+  'markdown-it': '8.4.2',
+  'markdown-it-video': '0.6.3'
 })
